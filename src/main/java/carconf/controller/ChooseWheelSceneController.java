3c86ca202b1c7b79cfd0f3c158d89d2b3671168a
mslowiak <marcin.slowiak.007@gmail.com>
2017-12-29 19:49:03 +0100

    Added wheelScene

---
+++ src/main/java/carconf/controller/ChooseWheelSceneController.java
@@ -0,0 +1,60 @@
+package carconf.controller;
+
+import carconf.App;
+import carconf.element.WheelInfo;
+import carconf.entity.Wheel;
+import carconf.service.impl.WheelServiceImpl;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+
+import java.util.List;
+
+public class ChooseWheelSceneController {
+    private Scene scene;
+    private ToggleGroup toggleGroup;
+
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    private HBox colorsHBox;
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    private Button goNextButton;
+
+    @FXML
+    void initialize(){
+        toggleGroup = new ToggleGroup();
+
+        goBackButton.setOnAction(e -> {
+
+        });
+
+        goNextButton.setOnAction(e ->{
+
+        });
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void displayWheels(){
+        WheelServiceImpl wheelService = new WheelServiceImpl();
+        List<Wheel> wheelsByLevelId = wheelService.getWheelsByLevelId(App.levelId);
+        for(int i = 0; i < wheelsByLevelId.size(); ++i){
+            Wheel wheel = wheelsByLevelId.get(i);
+            WheelInfo wheelInfo = new WheelInfo(wheel);
+            wheelInfo.getRadioButton().setUserData(i);
+            wheelInfo.getRadioButton().setToggleGroup(toggleGroup);
+            colorsHBox.getChildren().add(wheelInfo);
+        }
+    }
+}
---
+++ src/main/java/carconf/scene/ChooseWheelScene.java
@@ -0,0 +1,41 @@
+package carconf.scene;
+
+import carconf.controller.ChooseWheelSceneController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class ChooseWheelScene {
+    private Scene scene;
+    private FXMLLoader loader;
+    private ChooseWheelSceneController chooseWheelSceneController;
+
+    public ChooseWheelScene(Scene scene) {
+        this.scene = scene;
+        setScene();
+        setController();
+    }
+
+    private void setController() {
+        chooseWheelSceneController = loader.getController();
+        chooseWheelSceneController.setScene(scene);
+    }
+
+    private void setScene() {
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/choose_wheel_scene.fxml"));
+
+        try {
+            VBox vBox = loader.load();
+            scene.setRoot(vBox);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ChooseWheelSceneController getChooseWheelSceneController() {
+        return chooseWheelSceneController;
+    }
+}
