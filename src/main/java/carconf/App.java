26bf02d179f3d04d72bde05795e120b17c5a4f10
mslowiak <marcin.slowiak.007@gmail.com>
2018-01-08 14:30:33 +0100

    Decorator testing functionality in main

--- src/main/java/carconf/App.java
+++ src/main/java/carconf/App.java
@@ -1,5 +1,10 @@
 package carconf;
 
+import carconf.car_assembling.CustomizedCar;
+import carconf.car_assembling.car_decorators.ColorCarDecorator;
+import carconf.car_assembling.car_decorators.ModelCarDecorator;
+import carconf.entity.Color;
+import carconf.entity.Model;
 import carconf.scene.StartScene;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -23,6 +28,15 @@ public class App extends Application{
     public static int levelId;
 
     public static void main(String[] args) {
+//        CustomizedCar cc = new CustomizedCar();
+//        Model model = new Model("Sandero", "Dacia", 1733, 1519, 4069);
+//        Color color = new Color(model, "Biel Alpejska", "niemetalizowany", 0, "www.onet.pl");
+//
+//        cc = new ColorCarDecorator(cc, color);
+//        System.out.println(cc.getCarContent().getColor().getDescription());
+//        cc = new ModelCarDecorator(cc, model);
+//        System.out.println(cc.getCarContent().getColor().getDescription());
+//        System.out.println(cc.getCarContent().getModel().getBrand());
         launch(args);
     }
 }
