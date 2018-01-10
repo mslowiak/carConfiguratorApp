6401e8d5e395a7ba5789f40f0206a70b6b1eeb1c
Andrzej <andrzej.knapik1@gmail.com>
2018-01-10 18:19:58 +0100

    Singleton implementation

--- src/main/java/carconf/HibernateUtil.java
+++ src/main/java/carconf/HibernateUtil.java
@@ -5,7 +5,18 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static HibernateUtil instance = null;
     private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private HibernateUtil () {}
+
+    public static HibernateUtil getInstance() {
+        if(instance == null) {
+            instance = new HibernateUtil();
+        }
+        return instance;
+    }
+
     private static SessionFactory buildSessionFactory() {
         try {
             return new Configuration().configure().buildSessionFactory();
