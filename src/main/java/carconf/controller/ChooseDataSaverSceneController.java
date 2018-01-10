aeb4f66d1bd14450c883e9a3b52383841b306ec9
Andrzej <andrzej.knapik1@gmail.com>
2018-01-10 19:58:29 +0100

    Added choose dataSaver Scene

---
+++ src/main/java/carconf/controller/ChooseDataSaverSceneController.java
@@ -0,0 +1,71 @@
+package carconf.controller;
+
+import carconf.App;
+import carconf.car_assembling.car_decorators.WheelCarDecorator;
+import carconf.scene.ChooseEquipmentElementsScene;
+import carconf.service.impl.WheelServiceImpl;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+
+import java.util.List;
+
+public class ChooseDataSaverSceneController {
+    private Scene scene;
+    private ToggleGroup radiosGroup;
+
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    private HBox saversHBox;
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    private Button endConfigButton;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void initialize(){
+        radiosGroup = new ToggleGroup();
+
+        goBackButton.setOnAction(e -> {
+
+        });
+
+        endConfigButton.setOnAction(e ->{
+            if(radiosGroup.getSelectedToggle() != null) {
+//                int wheelId = Integer.parseInt(radiosGroup.getSelectedToggle().getUserData().toString());
+//                ChooseEquipmentElementsScene chooseEquipmentElementsScene = new ChooseEquipmentElementsScene(scene);
+//                chooseEquipmentElementsScene.getChooseEquipmentElementsSceneController().displayElements();
+//                WheelServiceImpl wheelService = new WheelServiceImpl();
+//                App.car = new WheelCarDecorator(App.car, wheelService.getWheelByWheelId(wheelId).get(0));
+            }else{
+                errorLabel.setText("Musisz wybrać sposób zapisu danych o Twojej konfiguracji");
+            }
+        });
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void displayOptionForSavingConfiguration(){
+//        WheelServiceImpl wheelService = new WheelServiceImpl();
+//        List<Wheel> wheelsByLevelId = wheelService.getWheelsByLevelId(App.car.getCarContent().getEquipmentLevel().getLevelId());
+//        for(int i = 0; i < wheelsByLevelId.size(); ++i){
+//            Wheel wheel = wheelsByLevelId.get(i);
+//            WheelInfo wheelInfo = new WheelInfo(wheel);
+//            wheelInfo.getRadioButton().setUserData(i);
+//            wheelInfo.getRadioButton().setToggleGroup(radiosGroup);
+//            colorsHBox.getChildren().add(wheelInfo);
+//        }
+    }
+}
---
+++ src/main/java/carconf/scene/ChooseDataSaverScene.java
@@ -0,0 +1,42 @@
+package carconf.scene;
+
+import carconf.controller.ChooseDataSaverSceneController;
+import carconf.controller.ChooseWheelSceneController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class ChooseDataSaverScene {
+    private Scene scene;
+    private FXMLLoader loader;
+    private ChooseDataSaverSceneController chooseDataSaverSceneController;
+
+    public ChooseDataSaverScene(Scene scene) {
+        this.scene = scene;
+        setScene();
+        setController();
+    }
+
+    private void setController() {
+        chooseDataSaverSceneController = loader.getController();
+        chooseDataSaverSceneController.setScene(scene);
+    }
+
+    private void setScene() {
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/choose_dataSaver_scene.fxml"));
+
+        try {
+            VBox vBox = loader.load();
+            scene.setRoot(vBox);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ChooseDataSaverSceneController getDataSaverSceneController() {
+        return chooseDataSaverSceneController;
+    }
+}
