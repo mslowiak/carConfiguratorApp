008045cbdb42f733558ffce397c64d0c504036fc
mslowiak <marcin.slowiak.007@gmail.com>
2018-01-11 18:32:07 +0100

    Added skeleton of Facade pattern

---
+++ src/main/java/carconf/car_assembling/CarInformationFacade.java
@@ -0,0 +1,22 @@
+package carconf.car_assembling;
+
+import javafx.scene.image.Image;
+
+public class CarInformationFacade {
+
+    public StringBuilder getFullCarDescription(){
+        return null;
+    }
+
+    public StringBuilder getShortCarDescription(){
+        return null;
+    }
+
+    public StringBuilder getTotalPriceByText(){
+        return null;
+    }
+
+    public Image getCarPhoto(){
+        return null;
+    }
+}
