dc86cd3b66e719d5c71e52a99a38eaaf9cc87263
Andrzej <andrzej.knapik1@gmail.com>
2018-01-12 23:23:58 +0100

    Modified the PdfSimpleSaver

--- src/main/java/carconf/savers/PdfSimpleSaver.java
+++ src/main/java/carconf/savers/PdfSimpleSaver.java
@@ -1,8 +1,10 @@
 package carconf.savers;
 
+import com.itextpdf.text.Document;
+
 public class PdfSimpleSaver implements SaverInterface {
     @Override
     public void saveCarConfiguration() {
-
+        Document document = new Document();
     }
 }
